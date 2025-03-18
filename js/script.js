49834fff03a8529ac2361b6f5c39bfbe32d2ca46
devswarnali <166713710+devswarnali@users.noreply.github.com>
2025-03-18 15:21:48 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -3,17 +3,17 @@ $('.discount-slide').slick({
     arrow: true,
     slidesToShow: 3,
     slidesToScroll: 1,
-    autoplay: true,
+    // autoplay: true,
     autoplaySpeed: 3000,
     responsive: [
         {
-          breakpoint: 992,
+          breakpoint: 768,
           settings: {
             slidesToShow: 2,
           }
         },
         {
-          breakpoint: 768,
+          breakpoint: 461,
           settings: {
             slidesToShow: 1,
           }
